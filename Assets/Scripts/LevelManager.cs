92e3e6a1661a9a5fc00fcec7459323bcf9978e64
Andrew McGovern <winedark.mcgovern@gmail.com>
2018-02-21 17:00:19 -0600

    finished basic gameplay

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log("I want to quit!!");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	public void BrickDestroyed() {
+		if (Brick.breakableCount <= 0) {
+			LoadNextLevel();
+		}
+	}
+}
